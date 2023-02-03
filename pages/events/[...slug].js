07e5a31489223d079de425cc0ec932601469849f
jooyong-boo <qnwndyd159@naver.com>
2023-02-03 18:37:48 +0900

    Section4

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FilteredEventsPage(props) {
+    return (
+        <div>
+            <h1>Filtered Events</h1>
+        </div>
+    );
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AllEventsPage(props) {
+    return (
+        <div>
+            <h1>All Events</h1>
+        </div>
+    );
+}
+
+export default AllEventsPage;
