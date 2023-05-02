a372156c3e8c58d06d8123914a2a17eb86f29b0e
jooyong-boo <qnwndyd159@naver.com>
2023-05-02 22:13:13 +0900

    동적으로 경로 로딩하기

--- next-practice/Section5/.next/static/chunks/webpack.js
+++ next-practice/Section5/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a523b46450a9c47e2d8";
+/******/ 	var hotCurrentHash = "4aa23a0356264f88a461";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next-practice/Section5/pages/[pid].js
+++ next-practice/Section5/pages/[pid].js
@@ -5,9 +5,9 @@ import fs from "fs/promises";
 function ProductDetailPage(props) {
     const { loadedProduct } = props;
 
-    // if (!loadedProduct) {
-    //     return <p>Loaded...</p>;
-    // }
+    if (!loadedProduct) {
+        return <p>Loaded...</p>;
+    }
     return (
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -16,16 +16,26 @@ function ProductDetailPage(props) {
     );
 }
 
+async function getData() {
+    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return data;
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
 
     const productId = params.pid;
 
-    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-    const jsonData = await fs.readFile(filePath);
-    const data = JSON.parse(jsonData);
+    const data = await getData();
 
     const product = data.products.find((product) => product.id === productId);
+
+    if (!product) {
+        return { notFound: true };
+    }
 
     return {
         props: {
@@ -35,9 +45,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+    const data = await getData();
+
+    const ids = data.products.map((product) => product.id);
+    const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
     return {
-        paths: [{ params: { pid: "p1" } }],
-        fallback: "blocking",
+        paths: pathWithParams,
+        fallback: true,
     };
 }
 
