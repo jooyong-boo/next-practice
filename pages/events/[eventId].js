b77e21f30e0ac0f00a78f2ae5aff10472e167173
jooyong-boo <qnwndyd159@naver.com>
2023-05-05 22:29:57 +0900

    데이터 페칭 최적화

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,40 +1,56 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+import { getFeaturedEvents, getEventById } from "../../helpers/api-util";
 
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
+function EventDetailPage(props) {
+    const event = props.selectedEvent;
 
-function EventDetailPage() {
-  const router = useRouter();
+    if (!event) {
+        return (
+            <div className="center">
+                <p>Loading...</p>
+            </div>
+        );
+    }
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
-  if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
-  }
+}
 
-  return (
-    <Fragment>
-      <EventSummary title={event.title} />
-      <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
-      />
-      <EventContent>
-        <p>{event.description}</p>
-      </EventContent>
-    </Fragment>
-  );
+export async function getStaticPaths() {
+    const events = await getFeaturedEvents();
+    const paths = events.map((event) => ({ params: { eventId: event.id } }));
+    return {
+        paths,
+        fallback: true,
+    };
+}
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+    const event = await getEventById(eventId);
+    console.log(event);
+    return {
+        props: {
+            selectedEvent: event,
+        },
+        revalidate: 30,
+    };
 }
 
 export default EventDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+    return (
+        <div>
+            <EventList items={props.events} />
+        </div>
+    );
+}
 
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+export async function getStaticProps() {
+    const feautredEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events: feautredEvents,
+        },
+        revalidate: 1800,
+    };
 }
 
 export default HomePage;
